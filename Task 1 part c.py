ba9f8f4b03757d773397dfc922b9e01f3b3d5cfa
Lucas Malik <47810442+pixeldude41@users.noreply.github.com>
2022-11-15 20:12:51 +0000

    Update and rename Task 1 part c to Task 1 part c.py

--- Task 1 part c.py
+++ Task 1 part c.py
@@ -6,4 +6,5 @@ https://www.w3schools.com/python/python_operators.asp
 for user input 
 number_input = int(input(' enter a series of numbers etc : ')
 
+Also use `while` for loop calculation
 These are helpful methods to be used in the question
